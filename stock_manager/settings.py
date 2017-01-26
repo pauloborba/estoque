bebb90989c80a0fe68d7957d15d560f6edf1fffe
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-01-26 18:59:28 -0300

    V1.0

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -148,12 +148,11 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=300)
 DATABASES['default'].update(db_from_env)
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'bower_components'),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bower_components'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
