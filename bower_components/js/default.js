16b328e602be2f21739b612a2ee98a7e27a7211b
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-02-01 22:21:14 -0300

    Changing validation

--- bower_components/js/default.js
+++ bower_components/js/default.js
@@ -14,15 +14,6 @@ function isValid(strInp){
   return (strInp && strInp.length !== 0);
 }
 
-function validate_login(){
-  username = document.getElementById("usernameInput").value;
-  pwd = document.getElementById("pwdInput").value;
-  if(isValid(username) && isValid(pwd))
-    document.getElementById("loginForm").submit();
-  else
-    Materialize.toast('Senha ou Nome vazios!', 4000);
-}
-
 function validate_new_item(){
   item_name = document.getElementById("itemNameInput").value;
   category = document.getElementById("categoryInput").value;
@@ -30,14 +21,6 @@ function validate_new_item(){
     document.getElementById("newItemForm").submit();
   else if(isValid(item_name))
     Materialize.toast('Escolha uma categoria!', 4000);
-  else
-    Materialize.toast('Nome é obrigatório!', 4000);
-}
-
-function validate_new_category(){
-  category_name = document.getElementById("categoryNameInput").value;
-  if(isValid(category_name))
-    document.getElementById("newCategoryForm").submit();
   else
     Materialize.toast('Nome é obrigatório!', 4000);
 }
