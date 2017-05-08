0d75df83f499afd50672abfe87e4fb25d1d60a2f
PedroRossi <pgrr@cin.ufpe.br>
2017-05-08 11:54:40 -0300

    Ambiente para execução de testes inicialmente pronto, falta setar funções a serem executadas antes e após cada teste

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,18 @@
 from splinter import Browser
+from sys import platform
+import os
 
-executable_path = {'executable_path':'./features/driver/linux/phantomjs'}
+os_name = ''
+if platform == "linux" or platform == "linux2":
+    os_name = "linux"
+elif platform == "darwin":
+    os_name = "macos"
+elif platform == "win32":
+    os_name = "windows"
+phantomjs_path = os.path.join(os.getcwd(),'features','driver',os_name,'phantomjs')
+if os_name == "windows":
+    phantomjs_path += ".exe"
+executable_path = {'executable_path':phantomjs_path}
 
 def before_all(context):
     context.browser = Browser('phantomjs', **executable_path)
