a87537423e4100417ee4be88b749590b956ec7e8
Albertinin Mourato Santos <ams11@cin.ufpe.br>
2017-05-10 23:26:29 -0300

    test sketches implemented

---
+++ features/albertfeature/cadastrarpreco.feature
@@ -0,0 +1,33 @@
+Feature: Cadastrar precos
+
+  Scenario: Cadastrar valor para um item existente na loja
+    Given eu vejo o item "Arroz" na lista de itens cadastrados
+    Given eu vejo a loja "Extra" na lista de lojas cadastradas
+    Given eu estou na pagina de cadastramento de precos
+    When eu tento cadastrar o preco "5,00" do item "Arroz" na loja "Extra"
+    Then eu vejo uma mensagem informando que o valor foi cadastrado corretamente
+    Then eu vou para a pagina de precos cadastrados
+
+  Scenario: Cadastrar valor para item que não está disponível na loja escolhido
+  	Given eu estou na pagina de cadastramento de precos
+  	Given eu vejo o item "Milho" na lista de itens cadastrados
+  	Given eu vejo a loja "Bonzao" na lista de lojas cadastradas
+  	Given a loja "Bonzao" nao possui o item "Milho"
+  	When eu tento cadastrar o preco do item "Milho" na loja "Bonzao"
+  	Then eu vejo uma mensagem informando que a loja "Bonzao" nao possui o item "Milho"
+  	Then eu permaneco na pagina de cadastramento de precos
+
+  Scenario: Sobrescrever um preco já cadastrado
+  	Given o item "Sabao" esta cadastrado no sistema com o preço "7,00" na loja "Merc"
+  	Given o item "Feijao" esta cadastrado no sistema com o preço "10,00" na loja "Varejão"
+  	When eu tento cadastrar o preco "5,00" do item "Sabao" na loja "Merc"
+  	Then o sistema sobrescreve o preco do item "Sabao" na loja "Merc" para o valor "5,00"
+  	And o item "Feijao" continua com o valor "10,00" na loja "Varejao"
+
+  Scenario: Cancelar cadastramento de preco de produto
+  	Given o item "Leite" esta cadastrada no sistema
+  	Given a loja "Bom Preço" esta cadastrada no sistema
+  	Given o preco "3,00" está para o item "Leite" na loja "Bom Preco"
+  	Given o item "Leite" na loja "Bom Preco" ainda nao possui um preco
+  	When eu tento cancelar o cadastramento do item "Leite" com o valor "3,00" na loja "Bom Preco"
+  	Then a lista de preços nao é alterada
---
+++ features/albertfeature/steps/cadastrarpreco.py
@@ -0,0 +1,79 @@
+import string
+from behave import *
+from django.contrib import messages
+
+#  Scenario: Cadastrar valor para item que não está disponível na loja escolhido - GUI
+@given('Eu estou na pagina de cadastramento de precos')
+	def step_impl(context):
+	    br = context.browser
+	    br.visit(context.new_price_url)
+	    response = br.status_code
+	    assert response.code == 200
+	    assert br.url.endswith("/newItem/")
+
+@given('eu vejo o item "{it}" na lista de itens cadastrados')
+	def step_impl(context):
+		br = context.browser
+		itemList = br.find_by_id('itemList')
+		assert itemList.get(name = it) != null
+
+@given('eu vejo a loja "{it}" na lista de lojas cadastradas')
+	def step_impl(context):
+		br = context.browser
+		storeList = br.find_by_id('storeList')
+		assert storeList.get(name = it) != null
+
+@given('a loja "{loja}" nao possui o item "{item}"')
+	def step_impl(context):
+		br = context.browser
+		itemList = br.find_by_id('itemList')
+		storeList = br.find_by_id('storeList')
+		it = itemList.get(name = item)
+		st = storeList.get(name = loja)
+		assert !(controller.verifyItemExistence(it st))
+
+@when('eu tento cadastrar o preco "{preco}" do item "{item}" na loja "{loja}"')
+	def step_impl(context):
+		br = context.browser
+		itemList = br.find_by_id('itemList')
+		storeList = br.find_by_id('storeList')
+		editText = br.find_by_id('editText')
+		it = itemList.get(name = item)
+		st = storeList.get(name = loja)
+		it.select()
+		st.select()
+		editText.fill(preco)
+		button = br.find_by_id('createPrice')
+		button.click()
+
+@then('eu vejo uma mensagem informando que a loja "{loja}" nao possui o item "{item}"')
+	def step_impl(context): 
+		controller.showException(["pt-br", "A loja "+loja+" não possui o item "+item])
+
+@then('eu permaneco na pagina de cadastramento de precos')
+	def step_impl(context): 
+		br = context.browser
+	    response = br.status_code
+	    assert response.code == 200
+	    assert br.url.endswith("/newPrice/")
+
+
+
+#  Scenario : Sobrescrever um preco já cadastrado - Controller
+
+#esse given serve para os dois primeiros passos do cenario
+@given('O item "{item}" esta cadastrado no sistema com o preço "{preco}" na loja "{loja}"')
+	def step_impl(context): 
+		st =  controller.objects.getLojaByID(loja)
+		assert st != null
+		it = st.getItemByID(item)
+		assert it !=null
+		assert it.getPrice() == preco
+
+#o when desse cenario é igual o anterior
+@then('o sistema sobrescreve o preco do item "{item}" na loja "{loja}" para o valor "{preco}"')
+	def step_impl(context):
+		st = controller.objects.getLojaByID(loja)
+		it = st.getItemByID(item)
+		controller.setPrice(it, st, preco)
+		assert controler.getPrice(it, st) == preco
